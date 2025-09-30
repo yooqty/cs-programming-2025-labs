4a57424b9a6ba27f49d90dde5eeea7b5a4b0d441
yooqty <yooqtty@gmail.com>
2025-09-30 16:40:46 +1000

    lab1

---
+++ labs/lab1/lab1.py
@@ -0,0 +1,86 @@
+#1
+# ints = 5
+# floats = 4.8
+# strs = 'dsgoiajgo'
+# bools = False
+
+#2
+# name = 'Lera'
+# age = 17
+# print(name, age)
+
+#3
+# x = 342
+# y = 56.2
+# z = '43'
+# sum = x+y+int(z)
+# print(sum)
+
+#4
+# a = 3
+# b = 8
+# print((a + 4 * b) * (a - 3 * b ) + a**2)
+
+#5
+# side1 = int(input('Введите ширину прямоугольника: '))
+# side2 = int(input('Введите высоту прямоугольника: '))
+# print('Площадь: ' + str(side1 * side2))
+# print('Периметр: ' + str((side1 + side2)*2))
+
+#6
+# print('*    *    *')
+# print(' *  * *  *')
+# print('   *   *')
+
+#7
+# a = 2
+# b = 3
+# print(a+b, a-b, a*b, a/b, a//b, a**b, a%b, a>b, a<b, a<=b, a>=b, a==b, a!=b)
+
+#8
+# name = 'Lera'
+# age = 17
+# print(f'Меня зовут {name}, мне {age} лет.')
+
+#9
+# string = 'Съешь еще этих мягких французских булок, да выпей чаю'
+# z = string.split(' ')
+# a = z[0]
+# b = z[1]
+# c = z[2]
+# d = z[3]
+# e = z[4]
+# f = z[5]
+# g = z[6]
+# h = z[7]
+# i = z[8]
+# a1 = a + ' ' + b
+# b2 = c + ' ' + d
+# c2 = e + ' ' + f
+# d2 = g + ' ' + h
+# e2 = i + ' ' + a
+# again = ' '.join(z)
+# print(again)
+
+#10
+# netda = 'Нет! Да! ' * 4
+# print(netda)
+
+#11
+# tri = input('Введите 3 числа, разделенные запятой: ')
+# c = tri.split(',')
+# x = int(c[0])
+# y = int(c[1])
+# w = int(c[2])
+# h = (x + w) // y
+# print('Результат вычисления: ' + str(h))
+
+#12
+# s = input('Введите слово, содержащее не менее 10 символов: ')
+# print(s[:4])
+# reverseds = ''.join(reversed(s))
+# lasttwo1 = reverseds[:2]
+# lasttwo = ''.join(reversed(lasttwo1))
+# print(lasttwo)
+# print(s[3:8])
+# print(reverseds)
